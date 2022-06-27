af07224b97b9a4a98979912168b3f94ff7211fe9
krisnaprtma <102678117+krisnadena@users.noreply.github.com>
2022-06-27 21:16:28 +0800

    Add files via upload

---
+++ book_form.php
@@ -0,0 +1,27 @@
+<?php
+
+   $connection = mysqli_connect('localhost','root','','book_db');
+
+   if(isset($_POST['send'])){
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $phone = $_POST['phone'];
+      $address = $_POST['address'];
+      $location = $_POST['location'];
+      $guests = $_POST['guests'];
+      $arrivals = $_POST['arrivals'];
+      $leaving = $_POST['leaving'];
+
+      $request = " insert into book_form(name, email, phone, address, location, guests, arrivals, leaving) values('$name','$email','$phone','$address','$location','$guests','$arrivals','$leaving') ";
+      if(mysqli_query($conn,$request)){
+         mysqli_close($conn);
+         header('location:book.php');
+      }else{
+         echo mysqli_error($conn);
+      }
+
+   }else{
+      echo 'something went wrong please try again!';
+   }
+
+?>
---
+++ connect.php
@@ -0,0 +1,12 @@
+
+<?php
+ $host="localhost";
+ $user="root";
+ $password='';
+ $db='book_db';
+
+// Create connection
+    $conn = mysqli_connect($host, $user, $password,$db);
+
+
+?>
